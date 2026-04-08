92d6f8b2de8d17e86b4d443162ad6fcb910418ca
Gandla Mallika <gandlamallika07@gmail.com>
2026-04-08 15:34:19 +0530

    Add files via upload

---
+++ evenorodd.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+   int a;
+   printf("enter the number\n");
+   scanf("%d",&a);
+   if(a%2==1)
+   {
+       printf("if is odd\n");
+   }
+   else
+   {
+      printf("ifis even\n"); 
+   }
+}
